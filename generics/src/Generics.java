cdd8d8d89f0326a9637241be1724fba284067f7c
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-08-28 21:48:13 -0300

    Aplicando método de comparação de Strings

---
+++ generics/src/Generics.java
@@ -0,0 +1,19 @@
+public class Generics {
+
+    public static void main(String[] args) {
+        String a = "l";
+        String b = "a";
+        System.out.println("Maior entre a e b: " + maior(a, b));
+    }
+
+    private static String maior(String primeira, String segunda) {
+        int compararString = primeira.compareTo(segunda);
+        if(compararString > 0){
+            return primeira;
+        } else if(compararString <0){
+            return segunda;
+        } else {
+            return "As Strings são iguais";
+        }
+    }
+}
